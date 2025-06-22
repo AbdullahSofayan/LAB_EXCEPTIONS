7722949c52cbeca263a0d13afe32920ab21f259f
AbdullahSofayan <444101428@student.ksu.edu.sa>
2025-06-22 23:10:49 +0300

    Solve Exception Lab

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+        print("The operation is successful")
+    except NameError:
+        print("NameError: The variable used in the addition is not defined.")
+
+
+additoin(10, 20)
+
+
+
+
+
+
+
+
